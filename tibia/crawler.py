cbd698cf8c14a71dcaa16c3634917984ded8539d
Marcus Mann <mpereirassa@gmail.com>
2018-12-02 04:22:27 -0300

    return parse

--- tibia/crawler.py
+++ tibia/crawler.py
@@ -12,8 +12,7 @@ class Crawler:
         character_url = urljoin(self.tibia_url, "community/?subtopic=characters")
         params = self.config_params(character)
         response = self.downlaoder.post(character_url, data=params)
-        parsed = self.parser.parse(response.text)
-        self.save_data(parsed)
+        return self.parser.parse(response.text)
 
     def config_params(self, character):
         return {"name": character, "Submit.x": 0, "Submit.y": 0}
